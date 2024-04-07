ae73908f285c9218e595e9a700382b84e675b47e
Kaito Kannari <19970311kk@gmail.com>
2024-04-08 02:37:46 +0900

    update: ルーレット実装に伴う修正

--- lib/presentation/pages/roulette/roulette_screen.dart
+++ lib/presentation/pages/roulette/roulette_screen.dart
@@ -1,24 +1,91 @@
 //TODO: ルーレット画面
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:math';
 
-class RouletteScreen extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
+
+class RouletteScreen extends StatefulWidget {
+  const RouletteScreen({super.key});
+
+  @override
+  State<RouletteScreen> createState() => _RouletteScreen();
+}
+
+class _RouletteScreen extends State<RouletteScreen> {
+  /// Roulette values
+  List<String> Item = [];
+
+  /// Result of roulette
+  List<String> results = [];
+  int? selectedIndex;
+  StreamController<int> selected = StreamController<int>();
+
+  /// Initalize ItemList
+  @override
+  initState() {
+    super.initState();
+    Item = generateItemList();
+  }
+
+  @override
+  void dispose() {
+    selected.close();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ルーレット'),
+        title: const Text('Roulette!!!'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'ルーレット画面',
-            ),
-          ],
+      resizeToAvoidBottomInset: false,
+      body: Container(
+        height: 450,
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: FortuneWheel(
+                  animateFirst: true,
+                  selected: selected.stream,
+                  items:
+                      Item.map((e) => FortuneItem(child: Text('$e'))).toList(),
+                  onAnimationEnd: () {
+                    /// 選択された値があれば結果に追加する
+                    if (selectedIndex != null) {
+                      setState(() {
+                        /// 選択された値をresultsに追加する
+                        results.add(Item[selectedIndex!]);
+
+                        /// 次のスピンに備えてnullにする
+                        selectedIndex = null;
+                      });
+                    }
+                    Navigator.popAndPushNamed(context, "/home");
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
+}
+
+/// Itemlist generator
+List<String> generateItemList() {
+  int listLength = Random().nextInt(10) + 1;
+  String item = '';
+  List<String> itemList = [];
+
+  for (int i = 0; i < listLength + 1; i++) {
+    item += Random().nextInt(10).toString();
+    itemList.add(item);
+  }
+  return itemList;
 }
--- lib/presentation/widgets/cmn_floating_action_button.dart
+++ lib/presentation/widgets/cmn_floating_action_button.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
-import '../dialogs/roulette_dialog.dart';
+import '../pages/roulette/roulette_screen.dart';
+
 
 class CmnFloatingActionButton extends StatelessWidget {
   const CmnFloatingActionButton({super.key});
 
   @override
   Widget build(BuildContext context) {
+    ///TODO: ルーレットのアイコンはアニメーションにする
     return FloatingActionButton(
       onPressed: () {
         showDialog<void>(
             context: context,
             builder: (_) {
-              return RouletteDialog();
+              return RouletteScreen();
             });
       },
     );
