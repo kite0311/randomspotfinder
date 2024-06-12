103ac1945106cede3e4adb70665820475e7606c9
Kaito Kannari <19970311kk@gmail.com>
2024-06-12 12:23:55 +0900

    Update: 型の修正

--- lib/models/dto/searrch_config/user_search_config.dart
+++ lib/models/dto/searrch_config/user_search_config.dart
@@ -1,17 +1,16 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'user_search_config.freezed.dart';
-
 part 'user_search_config.g.dart';
 
 @freezed
 class UserSearchConfig with _$UserSearchConfig {
   const factory UserSearchConfig({
     /// 検索範囲
-    required int distance,
+    required double distance,
 
     /// 検索ジャンル
-    required String searchGenre,
+    required String genre,
   }) = _UserSearchConfig;
 
   factory UserSearchConfig.fromJson(Map<String, dynamic> json) =>
