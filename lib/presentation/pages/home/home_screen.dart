caa1885e6b275bd285ebc66421236717cb6c5696
Kaito Kannari <19970311kk@gmail.com>
2024-06-12 12:19:57 +0900

    Update:ドロップダウンとスライダーの追加

--- lib/presentation/pages/home/home_screen.dart
+++ lib/presentation/pages/home/home_screen.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:randomspotfinder/constant/types.dart';
-import 'package:randomspotfinder/presentation/pages/home/test.dart';
 import 'package:randomspotfinder/presentation/widgets/roulette_button.dart';
 
+import '../../../domain/state/genre_items/genre_items.dart';
+import '../../../domain/state/user_search_config_provider.dart';
 import '../../widgets/cmn_bottom_app_bar.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final isSelectedItem = Entertainment.entertainmentList;
+  Widget build(BuildContext context, WidgetRef ref) {
+    final userSearchConfig = ref.watch(userSearchConfigProvider);
 
     return Scaffold(
       appBar: AppBar(
@@ -19,91 +19,48 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('ホーム画面'),
-            DropDown(
-              onSelected: (selectedItems) {
-                var isSelectedItem = selectedItems;
-              },
-            ),
-            DropDownTest(),
+            Text('ホーム画面 ${userSearchConfig}'),
+            DistanceSelectSlider(),
+            GenreSelectDropDownButton(),
           ],
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: RouletteButton(isSelectedItem: isSelectedItem),
+      floatingActionButton: RouletteButton(userSearchConfig: userSearchConfig),
       bottomNavigationBar: CmnBottomAppBar(),
     );
   }
 }
 
-/// ドロップダウンメニュー
-class DropDown extends StatefulWidget {
-  final Function(List<String>) onSelected;
-
-  const DropDown({super.key, required this.onSelected});
-
-  @override
-  State<DropDown> createState() => _DropDownState();
-}
-
-class _DropDownState extends State<DropDown> {
-  List<String> isSelectedItem = Entertainment.entertainmentList;
-
-  @override
-  Widget build(BuildContext context) {
-    return DropdownButton<List<String>>(
-      items: const [
-        DropdownMenuItem(
-            value: Entertainment.entertainmentList, child: Text('エンタメ')),
-        DropdownMenuItem(
-            value: FoodAndDrink.foodAndDrinkList, child: Text('飲食')),
-        DropdownMenuItem(value: Shopping.shoppingList, child: Text('ショッピング')),
-        DropdownMenuItem(
-            value: HealthAndWellness.healthAndWellnessList,
-            child: Text('健康・美容')),
-        DropdownMenuItem(value: Services.servicesList, child: Text('サービス')),
-        DropdownMenuItem(
-            value: Transportation.transportationList, child: Text('交通')),
-        DropdownMenuItem(
-            value: EducationAndGovernment.educationAndGovernmentList,
-            child: Text('教育・公共')),
-        DropdownMenuItem(
-            value: Miscellaneous.miscellaneousList, child: Text('その他')),
-      ],
-      value: isSelectedItem,
-      onChanged: (List<String>? newValue) {
-        setState(
-          () {
-            isSelectedItem = newValue!;
-          },
-        );
-        widget.onSelected(isSelectedItem!);
-      },
-    );
-  }
-}
-
-class DropDownTest extends ConsumerWidget {
-  const DropDownTest({super.key});
+//ドロップダウンボタン
+class GenreSelectDropDownButton extends ConsumerWidget {
+  const GenreSelectDropDownButton({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final testRef = ref.watch(testNotifierProvider);
+    final List<String> genreList = ref.watch(genreItemsNotifierProvider);
+    final String? selectedItem =
+        ref.watch(genreItemsNotifierProvider.notifier).selectedItem;
 
-    return DropdownButton<String>(
+    return DropdownButton(
+      value: selectedItem ?? genreList.first,
       icon: const Icon(Icons.arrow_downward),
-      elevation: 16,
-      style: const TextStyle(color: Colors.deepPurple),
+      elevation: 10,
+      style: const TextStyle(color: Colors.green),
       underline: Container(
-        height: 2,
-        color: Colors.deepPurpleAccent,
+        height: 4,
+        color: Colors.black,
       ),
       onChanged: (String? value) {
-        // This is called when the user selects an item.
-        ref.read(testNotifierProvider.notifier).updateState(value!);
+        if (value != null) {
+          ref.read(genreItemsNotifierProvider.notifier).selectItem(value);
+          ref.read(userSearchConfigProvider.notifier).updateGenre(value);
+          print(selectedItem);
+        }
       },
-      items: testRef.map<DropdownMenuItem<String>>((String value) {
+      items: genreList.map<DropdownMenuItem<String>>((String value) {
         return DropdownMenuItem<String>(
           value: value,
           child: Text(value),
@@ -112,3 +69,43 @@ class DropDownTest extends ConsumerWidget {
     );
   }
 }
+
+// スライダー
+class DistanceSelectSlider extends ConsumerWidget {
+  const DistanceSelectSlider({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    double _currentSliderValue = ref.watch(_DistanceProvider);
+
+    return Slider(
+      value: _currentSliderValue,
+      max: 50,
+      // 1,3,5,7,10,15,20,30,40,50の10等
+      divisions: 10,
+      label: _currentSliderValue.round().toString(),
+      onChanged: (double value) {
+        _currentSliderValue =
+            ref.read(_DistanceProvider.notifier).updateDistance(value);
+        ref.read(userSearchConfigProvider.notifier).updateDistance(value);
+        print(ref.read(_DistanceProvider.notifier).calcDistanceToMeter(value));
+      },
+    );
+  }
+}
+
+class distanceNotifier extends StateNotifier<double> {
+  distanceNotifier() : super(0.0);
+
+  updateDistance(double value) {
+    state = value;
+    return state;
+  }
+
+  double calcDistanceToMeter(double value) {
+    return value * 1000;
+  }
+}
+
+final _DistanceProvider = StateNotifierProvider<distanceNotifier, double>(
+    (ref) => distanceNotifier());
